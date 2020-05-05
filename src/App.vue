762f448a988f1d1f466e5a3ade628c33ccc19bd5
lilian <1508347793@qq.com>
2020-05-05 18:28:48 +0800

    cnode

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app" class="app">
+    <Header></Header>
+    <div class="main">
+      <router-view name="slidebar"></router-view>
+      <router-view name="main"></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+import Postlist from "./components/Postlist";
+
+
+export default {
+  name: 'App',
+  components:{
+    Header,
+    Postlist,
+  }
+}
+</script>
+
+<style scoped>
+.app{
+  background: #E2E0E1;
+
+}
+.main{
+  width: 80%;
+  margin: 0 auto;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Article from '../components/Article'
+import Postlist from '../components/Postlist'
+import UserInfo from '../components/UserInfo'
+import SlideBar from '../components/SlideBar'
+
+
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'root',
+      components:{
+        main:Postlist,
+      }
+    },
+    {
+      path: '/topic/:id&author=:name',
+      name: 'post_content',
+      components:{
+        main:Article,
+        slidebar:SlideBar
+
+      }
+    },
+    {
+      path: '/userinfo/:name',
+      name: 'user_info',
+      components:{
+        main:UserInfo,
+      }
+    },
+  ]
+})
